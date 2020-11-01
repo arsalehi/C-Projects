5fae9bfd293b0b7d40be916d8930938a6253cfba
Ali Reza Salehi <arsalehi@unix4.lt.ucsc.edu>
2020-10-31 18:35:43 -0700

    Files after clang format

--- asgn3/stack.c
+++ asgn3/stack.c
@@ -4,69 +4,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Stack *stack_create(int capacity, char name){
-    Stack *s = (Stack *)malloc(sizeof(Stack));
+Stack *stack_create(int capacity, char name) {
+  Stack *s = (Stack *)malloc(sizeof(Stack));
 
-    if (!s) {
-        return 0;
-    }
+  if (!s) {
+    return 0;
+  }
 
-    if (capacity < 1){
-        capacity = 1;
-    }
+  if (capacity < 1) {
+    capacity = 1;
+  }
 
-    s->name = name;
-    s->capacity = capacity;
-    s->top = 0;
+  s->name = name;
+  s->capacity = capacity;
+  s->top = 0;
 
-    s-> items = (int *)malloc(sizeof(int) * capacity);
+  s->items = (int *)malloc(sizeof(int) * capacity);
 
-    if(!s ->items){
-        return 0;
-    }
+  if (!s->items) {
+    return 0;
+  }
 
-    return s;
+  return s;
 }
 
-void stack_delete(Stack *s){
-    free(s-> items);
-    free(s);
+void stack_delete(Stack *s) {
+  free(s->items);
+  free(s);
+  return;
+}
+
+int stack_pop(Stack *s) {
+  if (!s) {
+    return -1;
+  }
+
+  if (s->top > 0) {
+    s->top -= 1;
+    return s->items[s->top];
+  } else {
+    return -1;
+  }
+}
+
+void stack_push(Stack *s, int item) {
+  if (!s) {
     return;
+  }
+  if (s->top == s->capacity) {
+    s->capacity *= 2;
+    s->items = (int *)realloc(s->items, s->capacity * sizeof(int));
+  }
+  if (s->items) {
+    s->items[s->top] = item;
+    s->top += 1;
+  }
+  return;
 }
 
-int stack_pop(Stack *s){
-    if (!s){
-        return -1;
-    }
+bool stack_empty(Stack *s) { return s->top == 0; }
 
-    if (s-> top > 0){
-        s-> top -= 1;
-        return s-> items[s-> top];
-    } else {
-        return -1;
-    }
-}
-
-void stack_push(Stack *s, int item){
-    if(!s){
-        return;
-    }
-    if (s->top == s-> capacity){
-        s->capacity *= 2;
-        s->items = (int *)realloc(s->items, s->capacity * sizeof(int));
-    }
-    if (s->items) {
-        s->items[s->top] = item;
-        s->top += 1;
-    }
-    return;
-}
-
-bool stack_empty(Stack *s){
-    return s-> top == 0;
-}
-
-int stack_peek(Stack *s){
-    return s->top;
-}
-
+int stack_peek(Stack *s) { return s->top; }
